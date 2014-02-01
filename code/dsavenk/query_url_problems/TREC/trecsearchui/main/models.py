560fd83d7d7f2d674917f91c08db76c8cd70fa98
Denis Savenkov <dsavenk@emory.edu>
2014-01-31 20:14:32 -0500

    Judging was added

--- code/dsavenk/query_url_problems/TREC/trecsearchui/main/models.py
+++ code/dsavenk/query_url_problems/TREC/trecsearchui/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Problem(models.Model):
+	topic = models.PositiveIntegerField()
+	query = models.CharField(max_length=255)
+	docid = models.PositiveIntegerField()
+	docno = models.CharField(max_length=32)
+	problem = models.TextField()
+	flag = models.PositiveIntegerField()
--- code/dsavenk/query_url_problems/TREC/trecsearchui/main/views.py
+++ code/dsavenk/query_url_problems/TREC/trecsearchui/main/views.py
@@ -2,19 +2,24 @@
 from django.shortcuts import render_to_response
 from utils import *
 from trecsearchui.settings import QRELSFILE, TOPICSFILE
-
+from models import Problem
 
 def main(request):
     context = {}
     topics = read_topics(TOPICSFILE)
     context["topics"] = [topics[key] for key in sorted(topics.keys())]
-    if request.GET.has_key("q"):
-        query = request.GET["q"]
-        topic = int(request.GET["t"])
+    if request.POST.has_key("q"):
+        query = request.POST["q"]
+        topic = int(request.POST["t"])
         qrels = read_qrels(QRELSFILE, topic)
         if len(query) == 0:
             query = topics[topic]["title"]
-        results = get_search_results(1000, True, query)
+        results = get_search_results(10, True, query)
+        problems = request.POST.getlist('problem')
+        for rank, problem in enumerate(problems):
+            Problem.objects.create(topic=topic, query=query, docid=int(results[rank][0]),
+                docno=results[rank][1], problem=problem, flag=0)
+
         results1000 = get_search_results(1000, False, query)
         display_results = []
         for res in results:
