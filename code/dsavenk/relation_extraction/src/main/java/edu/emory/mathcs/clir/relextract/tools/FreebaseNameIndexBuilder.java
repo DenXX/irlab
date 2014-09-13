026bb3802552601f8c18b6bf893e2fd411df955c
DenisSavenkov <denissavenkov@gmail.com>
2014-09-13 16:17:39 -0400

    Switching from Apache Jena to regular file reading for RDF parsing

--- code/dsavenk/relation_extraction/src/main/java/edu/emory/mathcs/clir/relextract/tools/FreebaseNameIndexBuilder.java
+++ code/dsavenk/relation_extraction/src/main/java/edu/emory/mathcs/clir/relextract/tools/FreebaseNameIndexBuilder.java
@@ -1,7 +1,5 @@
 package edu.emory.mathcs.clir.relextract.tools;
 
-import com.hp.hpl.jena.datatypes.DatatypeFormatException;
-import com.hp.hpl.jena.graph.Triple;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
@@ -11,16 +9,9 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.compress.compressors.CompressorException;
 import org.apache.commons.compress.compressors.CompressorStreamFactory;
-import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.riot.lang.PipedRDFIterator;
-import org.apache.jena.riot.lang.PipedRDFStream;
-import org.apache.jena.riot.lang.PipedTriplesStream;
 
 import java.io.*;
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Reads Freebase RDF data dump and extracts all entity names, then for each
@@ -48,82 +39,62 @@ public class FreebaseNameIndexBuilder {
         return parser.parse(options, args);
     }
 
+    private static String stripFreebaseRdfNamespace(String rdfUri) {
+        return rdfUri.replace("<http://rdf.freebase.com/ns", "")
+                .replace(">", "").replace(".", "/");
+    }
+
     private static void processFreebaseDump(final String freebase_dump_file,
                                             String outputFile) {
-        PipedRDFIterator<Triple> iter = new PipedRDFIterator<Triple>();
-        final PipedRDFStream<Triple> triplesStream =
-                new PipedTriplesStream(iter);
-
-        // PipedRDFStream and PipedRDFIterator need to be on different threads
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-
-        // Create a runnable for our parser thread
-        Runnable parser = new Runnable() {
-            @Override
-            public void run() {
-                // Call the parsing process.
-                try {
-                    RDFDataMgr.parse(triplesStream,
-                            new CompressorStreamFactory()
-                                    .createCompressorInputStream(
-                                            CompressorStreamFactory.GZIP,
-                                            new BufferedInputStream(
-                                                new FileInputStream(
-                                                    freebase_dump_file))),
-                            "http://rdf.freebase.com/ns/",
-                            Lang.TURTLE);
-                } catch (CompressorException e) {
-                    System.err.println("Cannot decompress file: " +
-                            e.getMessage());
-                    return;
-                } catch (FileNotFoundException e) {
-                    System.err.println("File not found: " + freebase_dump_file);
-                    return;
-                }
-            }
-        };
-
-        // Start the parser on another thread
-        executor.submit(parser);
-
         HashMap<String, Long> entityTriplesCount = new HashMap<String, Long>();
         HashMap<String, Set<String>> nameEntityIndex =
                 new HashMap<String, Set<String>>();
-        while (iter.hasNext()) {
-            Triple triple = iter.next();
-            String subject =
-                    "/" + triple.getSubject().getLocalName().replace(".", "/");
-            String predicate =
-                    "/" + triple.getPredicate().getLocalName()
-                            .replace(".", "/");
-            String object = null;
-            try {
-                if (triple.getObject().isLiteral()) {
-                    object = triple.getObject().getLiteralValue().toString();
-                } else if (triple.getObject().isVariable()) {
-                    object = "/" +
-                            triple.getObject().getLocalName().replace(".", "/");
-                } else {
-                    object = triple.getObject().toString();
-                }
-                if (!entityTriplesCount.containsKey(subject)) {
-                    entityTriplesCount.put(subject, 1L);
-                } else {
-                    entityTriplesCount.put(subject,
-                            entityTriplesCount.get(subject) + 1L);
-                }
 
-                if (subject.startsWith("/m/") &&
-                        (predicate.equals("/type/object/name") ||
-                                predicate.equals("/common/topic/alias"))) {
-                    if (!nameEntityIndex.containsKey(object)) {
-                        nameEntityIndex.put(object, new HashSet<String>());
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(
+                    new CompressorStreamFactory().createCompressorInputStream(
+                            CompressorStreamFactory.GZIP,
+                            new BufferedInputStream(
+                                    new FileInputStream(
+                                            freebase_dump_file)))));
+        } catch (CompressorException e) {
+            System.err.println("Cannot decompress file: " +
+                    e.getMessage());
+            System.exit(-1);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + freebase_dump_file);
+            System.exit(-1);
+        }
+
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                String[] triple = line.split("\t");
+                String subject = stripFreebaseRdfNamespace(triple[0]);
+                String object = stripFreebaseRdfNamespace(triple[2]);
+                String predicate = stripFreebaseRdfNamespace(triple[1]);
+
+                if (subject.startsWith("/m/")) {
+                    if (!entityTriplesCount.containsKey(subject)) {
+                        entityTriplesCount.put(subject, 1L);
+                    } else {
+                        entityTriplesCount.put(subject,
+                                entityTriplesCount.get(subject) + 1L);
                     }
-                    nameEntityIndex.get(object).add(subject);
+
+                    if (predicate.equals("/type/object/name") ||
+                                    predicate.equals("/common/topic/alias")) {
+                        if (!nameEntityIndex.containsKey(object)) {
+                            nameEntityIndex.put(object, new HashSet<String>());
+                        }
+                        nameEntityIndex.get(object).add(subject);
+                    }
                 }
-            } catch (DatatypeFormatException e) {
-                System.err.println("Error: " + e.getMessage());
             }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            System.exit(-1);
         }
 
         PrintWriter out = null;
@@ -150,7 +121,6 @@ public class FreebaseNameIndexBuilder {
             }
             out.println();
         }
-        executor.shutdown();
         out.close();
     }
 
