08bbea92fe8ecd0959a431b218772394ad2f0c63
DenisSavenkov <denissavenkov@gmail.com>
2014-11-26 16:52:51 -0500

    Experiment to always get best non-NONE prediction if probability is not 0.

--- code/dsavenk/relation_extraction/src/main/java/edu/emory/mathcs/clir/relextract/utils/RelationExtractorModelTrainer.java
+++ code/dsavenk/relation_extraction/src/main/java/edu/emory/mathcs/clir/relextract/utils/RelationExtractorModelTrainer.java
@@ -112,7 +112,7 @@ public class RelationExtractorModelTrainer {
         edu.stanford.nlp.classify.Dataset<String, Integer> dataset = convertDataset(testDataset, true, 1.0f);
         for (Datum<String, Integer> example : dataset) {
             Counter<String> predictions = model.probabilityOf(example);
-            double bestScore = 1.0 / testDataset.getLabelCount(); //predictions.getCount("NONE");
+            double bestScore = 0.000001; //1.0 / testDataset.getLabelCount(); //predictions.getCount("NONE");
             String bestLabel = "NONE";
             for (Map.Entry<String, Double> pred : predictions.entrySet()) {
                 if (!pred.getKey().equals("NONE") && !pred.getKey().equals("OTHER")) {
