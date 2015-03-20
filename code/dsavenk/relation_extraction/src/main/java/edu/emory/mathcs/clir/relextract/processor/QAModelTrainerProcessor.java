3c039c99372a3072ed09eb301af300f6f97f59fb
DenisSavenkov <denissavenkov@gmail.com>
2015-03-20 18:25:01 -0400

    Filter out QA pairs without positive instances

--- code/dsavenk/relation_extraction/src/main/java/edu/emory/mathcs/clir/relextract/processor/QAModelTrainerProcessor.java
+++ code/dsavenk/relation_extraction/src/main/java/edu/emory/mathcs/clir/relextract/processor/QAModelTrainerProcessor.java
@@ -13,6 +13,7 @@ import edu.emory.mathcs.clir.relextract.utils.DependencyTreeUtils;
 import edu.emory.mathcs.clir.relextract.utils.DocumentUtils;
 import edu.emory.mathcs.clir.relextract.utils.KnowledgeBase;
 import edu.stanford.nlp.classify.Dataset;
+import edu.stanford.nlp.ling.Datum;
 import edu.stanford.nlp.util.Interval;
 import edu.stanford.nlp.util.IntervalTree;
 import org.apache.xerces.impl.dv.XSSimpleType;
@@ -28,6 +29,23 @@ public class QAModelTrainerProcessor extends Processor {
     private final Dataset<Boolean, String> dataset_ = new Dataset<>();
     private final KnowledgeBase kb_;
     private Random rnd_ = new Random(42);
+
+    String[] feat1 = {"qword", "arelation"};
+    String[] feat2 = {"qword", "atopic"};
+    String[] feat3 = {"qword", "atopic", "arelation"};
+    String[] feat4 = {"qverb", "arelation"};
+    String[] feat5 = {"qverb", "atopic"};
+    String[] feat6 = {"qverb", "atopic", "arelation"};
+    String[] feat7 = {"qfocus", "arelation"};
+    String[] feat8 = {"qfocus", "atopic"};
+    String[] feat9 = {"qfocus", "atopic", "arelation"};
+    String[] feat10 = {"qtopic", "arelation"};
+    String[] feat11 = {"qtopic", "atopic"};
+    String[] feat12 = {"qtopic", "atopic", "arelation"};
+    String[] feat13 = {"qword", "qverb", "qfocus", "qtopic", "arelation"};
+    String[] feat14 = {"qword", "qverb", "qfocus", "qtopic", "atopic"};
+    String[] feat15 = {"qword", "qverb", "qfocus", "qtopic", "atopic", "arelation"};
+    String[] feat16 = {"qdeppath", "arelation"};
 
     /**
      * Processors can take parameters, that are stored inside the properties
@@ -70,7 +88,7 @@ public class QAModelTrainerProcessor extends Processor {
 
         Set<String> features = new HashSet<>();
         for (Document.QaRelationInstance instance : document.getQaInstanceList()) {
-            if (!instance.getIsPositive() & rnd_.nextInt(100) > 10) {
+            if (!instance.getIsPositive() & rnd_.nextInt(100) > 5) {
                 continue;
             }
             features.clear();
@@ -110,34 +128,34 @@ public class QAModelTrainerProcessor extends Processor {
         Set<String> qFocuses = document.getQuestionFocus();
 
         for (String qWord : qWords) {
-            features.add(String.format("qword=%s|arelation=%s", qWord, instance.getPredicate()));
+            features.add(getFeature(feat1, qWord, instance.getPredicate()));
             for (String answerEntityType : answerEntityTypes) {
-                features.add(String.format("qword=%s|atopic=%s", qWord, answerEntityType));
-                features.add(String.format("qword=%s|atopic=%s|arelation=%s", qWord, answerEntityType, instance.getPredicate()));
+                features.add(getFeature(feat2, qWord, answerEntityType));
+                features.add(getFeature(feat3, qWord, answerEntityType, instance.getPredicate()));
             }
         }
 
         for (String qVerb : qVerbs) {
-            features.add(String.format("qverb=%s|arelation=%s", qVerb, instance.getPredicate()));
+            features.add(getFeature(feat4, qVerb, instance.getPredicate()));
             for (String answerEntityType : answerEntityTypes) {
-                features.add(String.format("qverb=%s|atopic=%s", qVerb, answerEntityType));
-                features.add(String.format("qverb=%s|atopic=%s|arelation=%s", qVerb, answerEntityType, instance.getPredicate()));
+                features.add(getFeature(feat5, qVerb, answerEntityType));
+                features.add(getFeature(feat6, qVerb, answerEntityType, instance.getPredicate()));
             }
         }
 
         for (String qFocus : qFocuses) {
-            features.add(String.format("qfocus=%s|arelation=%s", qFocus, instance.getPredicate()));
+            features.add(getFeature(feat7, qFocus, instance.getPredicate()));
             for (String answerEntityType : answerEntityTypes) {
-                features.add(String.format("qfocus=%s|atopic=%s", qFocus, answerEntityType));
-                features.add(String.format("qfocus=%s|atopic=%s|arelation=%s", qFocus, answerEntityType, instance.getPredicate()));
+                features.add(getFeature(feat8, qFocus, answerEntityType));
+                features.add(getFeature(feat9, qFocus, answerEntityType, instance.getPredicate()));
             }
         }
 
         for (String questionEntityType : questionEntityTypes) {
-            features.add(String.format("qtopic=%s|arelation=%s", questionEntityType, instance.getPredicate()));
+            features.add(getFeature(feat10, questionEntityType, instance.getPredicate()));
             for (String answerEntityType : answerEntityTypes) {
-                features.add(String.format("qtopic=%s|atopic=%s", questionEntityType, answerEntityType));
-                features.add(String.format("qtopic=%s|atopic=%s|arelation=%s", questionEntityType, answerEntityType, instance.getPredicate()));
+                features.add(getFeature(feat11, questionEntityType, answerEntityType));
+                features.add(getFeature(feat12, questionEntityType, answerEntityType, instance.getPredicate()));
             }
         }
 
@@ -145,10 +163,10 @@ public class QAModelTrainerProcessor extends Processor {
             for (String qVerb : qVerbs) {
                 for (String qFocus : qFocuses) {
                     for (String questionEntityType : questionEntityTypes) {
-                        features.add(String.format("qword=%s|qverb=%s|qfocus=%s|qtopic=%s|arelation=%s", qWord, qVerb, qFocus, questionEntityType, instance.getPredicate()));
+                        features.add(getFeature(feat13, qWord, qVerb, qFocus, questionEntityType, instance.getPredicate()));
                         for (String answerEntityType : answerEntityTypes) {
-                            features.add(String.format("qword=%s|qverb=%s|qfocus=%s|qtopic=%s|atopic=%s", qWord, qVerb, qFocus, questionEntityType, answerEntityType));
-                            features.add(String.format("qword=%s|qverb=%s|qfocus=%s|qtopic=%s|atopic=%s|arelation=%s", qWord, qVerb, qFocus, questionEntityType, answerEntityType, instance.getPredicate()));
+                            features.add(getFeature(feat14, qWord, qVerb, qFocus, questionEntityType, answerEntityType));
+                            features.add(getFeature(feat15, qWord, qVerb, qFocus, questionEntityType, answerEntityType, instance.getPredicate()));
                         }
                     }
                 }
@@ -157,7 +175,7 @@ public class QAModelTrainerProcessor extends Processor {
 
         for (String depPath : qDepPaths) {
             for (String questionEntityType : questionEntityTypes) {
-                features.add(String.format("qdeppath=%s%s|arelation=%s", depPath, questionEntityType, instance.getPredicate()));
+                features.add(getFeature(feat16, depPath, questionEntityType, instance.getPredicate()));
             }
         }
 
@@ -178,6 +196,16 @@ public class QAModelTrainerProcessor extends Processor {
 //                }
 //            }
 //        }
+    }
+
+    private String getFeature(String[] parts, String... valParts) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < parts.length; ++i) {
+            res.append(parts[i]);
+            res.append("|");
+            res.append(valParts[i]);
+        }
+        return res.toString();
     }
 }
 
