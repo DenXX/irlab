bbfc61a5294894da392ec12b2e83e3512d98507a
DenisSavenkov <denissavenkov@gmail.com>
2015-08-28 16:48:37 -0400

    Removing Knowledge base class for now.

--- code/dsavenk/liveqa/src/main/java/edu/emory/mathcs/ir/utilapps/Question2QueryTestApp.java
+++ code/dsavenk/liveqa/src/main/java/edu/emory/mathcs/ir/utilapps/Question2QueryTestApp.java
@@ -1,7 +1,5 @@
 package edu.emory.mathcs.ir.utilapps;
 
-import edu.emory.mathcs.ir.kb.KnowledgeBase;
-import edu.emory.mathcs.ir.qa.AppConfig;
 import edu.emory.mathcs.ir.qa.Question;
 import edu.emory.mathcs.ir.qa.answerer.query.QueryFormulation;
 import edu.emory.mathcs.ir.qa.answerer.query.TestQueryFormulator;
@@ -13,7 +11,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.FileSystems;
-import java.util.Set;
 
 /**
  * Created by dsavenk on 8/26/15.
@@ -27,12 +24,6 @@ public class Question2QueryTestApp {
                         FileSystems.getDefault().getPath(args[0])));
         QueryFormulation queryFormulation =
                 new TestQueryFormulator(indexReader);
-
-        KnowledgeBase kb = KnowledgeBase.getInstance(
-                AppConfig.PROPERTIES.getProperty(AppConfig.KB_MODEL_PARAMETER),
-                AppConfig.PROPERTIES.getProperty(AppConfig.KB_INDEX_PARAMETER));
-
-        Set<String> mids = kb.lookupEntitiesByName("hair");
 
         String line;
         while ((line = input.readLine()) != null) {
