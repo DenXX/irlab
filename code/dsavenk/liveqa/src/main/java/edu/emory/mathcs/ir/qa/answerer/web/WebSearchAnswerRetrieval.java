1685882cc692c4a5ad961ca75d26f89ed4a2cc11
DenisSavenkov <denissavenkov@gmail.com>
2015-08-31 22:11:14 -0400

    Making query processing parallel

--- code/dsavenk/liveqa/src/main/java/edu/emory/mathcs/ir/qa/answerer/web/WebSearchAnswerRetrieval.java
+++ code/dsavenk/liveqa/src/main/java/edu/emory/mathcs/ir/qa/answerer/web/WebSearchAnswerRetrieval.java
@@ -52,47 +52,51 @@ public class WebSearchAnswerRetrieval implements AnswerRetrieval {
 
     @Override
     public Answer[] retrieveAnswers(Question question) {
-        List<Answer> answers = new ArrayList<>();
-        for (final QueryFormulation queryFormulator : queryFormulators_) {
-            // Get search results for the query.
-            final String query = queryFormulator.getQuery(question);
-            final SearchResult[] results = search_.search(query, topN_);
+        return Arrays.stream(queryFormulators_)
+                .parallel()
+                .flatMap(queryFormulator -> {
+                    List<Answer> answers = new ArrayList<>();
+                    // Get search results for the query.
+                    final String query = queryFormulator.getQuery(question);
+                    final SearchResult[] results = search_.search(query, topN_);
 
-            // Process results and add content to each document.
-            Arrays.stream(results)
-                    .parallel()
-                    .forEach(res -> {
-                        try {
-                            res.content = WebPageScraper.getDocumentContent(
-                                    new URL(res.url));
-                        } catch (Exception e) {
-                            LiveQaLogger.LOGGER.warning(e.getMessage());
-                        }
-                    });
+                    // Process results and add content to each document.
+                    Arrays.stream(results)
+                            .parallel()
+                            .forEach(res -> {
+                                try {
+                                    res.content =
+                                            WebPageScraper.getDocumentContent(
+                                                    new URL(res.url));
+                                } catch (Exception e) {
+                                    LiveQaLogger.LOGGER.warning(e.getMessage());
+                                }
+                            });
 
-            // Generate passages from all retrieved documents.
-            for (SearchResult result : results) {
-                // Generate passages from the current documents.
-                final Text[] currentPassages =
-                        passageRetrieval_.getPassages(
-                                new Text(AnswerFormatter.formatAnswer(
-                                        result.content)), maxLength_);
+                    // Generate passages from all retrieved documents.
+                    for (SearchResult result : results) {
+                        // Generate passages from the current documents.
+                        final Text[] currentPassages =
+                                passageRetrieval_.getPassages(
+                                        new Text(AnswerFormatter.formatAnswer(
+                                                result.content)), maxLength_);
 
-                // Add passages to the list of answers.
-                Arrays.stream(currentPassages)
-                        .map(p -> new Answer(p, result.url))
-                        .map(a -> {
-                            a.setAttribute(PAGE_TITLE_ATTRIBUTE, result.title);
-                            return a;
-                        })
-                        .forEach(answers::add);
-                Answer snippetAnswer = new Answer(
-                        AnswerFormatter.formatAnswer(result.snippet),
-                        result.url);
-                snippetAnswer.setAttribute(PAGE_TITLE_ATTRIBUTE, result.title);
-                answers.add(snippetAnswer);
-            }
-        }
-        return answers.toArray(new Answer[answers.size()]);
+                        // Add passages to the list of answers.
+                        Arrays.stream(currentPassages)
+                                .map(p -> new Answer(p, result.url))
+                                .map(a -> {
+                                    a.setAttribute(PAGE_TITLE_ATTRIBUTE, result.title);
+                                    return a;
+                                })
+                                .forEach(answers::add);
+                        Answer snippetAnswer = new Answer(
+                                AnswerFormatter.formatAnswer(result.snippet),
+                                result.url);
+                        snippetAnswer.setAttribute(PAGE_TITLE_ATTRIBUTE, result.title);
+                        answers.add(snippetAnswer);
+                    }
+                    return answers.stream();
+                })
+                .toArray(Answer[]::new);
     }
 }
