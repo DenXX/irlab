c700c7f059b6621532a7f19ec2d9273104cbf1d4
DenisSavenkov <denissavenkov@gmail.com>
2015-09-07 11:59:27 -0400

    Adding another type of LSTM model

--- code/dsavenk/deep_learning_experiments/lstm/lstm_sequence_test.py
+++ code/dsavenk/deep_learning_experiments/lstm/lstm_sequence_test.py
@@ -7,7 +7,7 @@ import sys
 import numpy as np
 import random
 
-from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.core import Dense, Activation, Merge
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
@@ -16,8 +16,8 @@ from keras.preprocessing.sequence import pad_sequences
 VOCABULARY_SIZE = 100
 EMBEDDING_DIMENSION = 64
 HIDDEN_DIMENSION = 32
-TRAINING_SIZE = 100000
-TEST_SIZE = 10000
+TRAINING_SIZE = 1000
+TEST_SIZE = 100
 BATCH_SIZE = 500
 EPOCHS = 5000
 
@@ -34,6 +34,18 @@ def generate_sequence(label, length):
     return seq
 
 
+def generate_sequence2(label, length):
+    seq = list(range(1, VOCABULARY_SIZE))
+    random.shuffle(seq)
+    seq1 = seq[:int(length / 2)]
+    seq2 = seq[int(length / 2): length]
+    if label:
+        repetition = random.choice(seq1)
+        position = random.randint(0, len(seq2) - 1)
+        seq2[position] = repetition
+    return seq1, seq2
+
+
 def generate_data(size):
     X = []
     y = []
@@ -44,7 +56,20 @@ def generate_data(size):
     return pad_sequences(X, maxlen=21), np.array(y)
 
 
-def main():
+def generate_data2(size):
+    X1 = []
+    X2 = []
+    y = []
+    for i in range(size):
+        label = random.random() > 0.5
+        X = generate_sequence2(label, 20)
+        X1.append(X[0])
+        X2.append(X[0])
+        y.append(1 if label else -1)
+    return pad_sequences(X1, maxlen=10), pad_sequences(X2, maxlen=10), np.array(y)
+
+
+def main_singlemodel():
     X, y = generate_data(TRAINING_SIZE)
     X_test, y_test = generate_data(TEST_SIZE)
 
@@ -52,7 +77,6 @@ def main():
     model = Sequential()
     model.add(Embedding(VOCABULARY_SIZE, EMBEDDING_DIMENSION))
     model.add(LSTM(EMBEDDING_DIMENSION, HIDDEN_DIMENSION))
-    #model.add(Dropout(0.5))
     model.add(Dense(HIDDEN_DIMENSION, 1))
     model.add(Activation('sigmoid'))
     print('Compiling...', file=sys.stderr)
@@ -65,7 +89,37 @@ def main():
     print("Testing...", file=sys.stderr)
     score, acc = model.evaluate(X_test, y_test, batch_size=BATCH_SIZE,
                                 show_accuracy=True)
-    print("Testing performance = " + score + ", acc = " + acc)
+    print("Testing performance = " + str(score) + ", acc = " + str(acc))
+
+
+def main_separatemodels():
+    X1, X2, y = generate_data2(TRAINING_SIZE)
+    X1_test, X2_test, y_test = generate_data2(TEST_SIZE)
+
+    print('Defining network...', file=sys.stderr)
+    firstlstm = Sequential()
+    firstlstm.add(Embedding(VOCABULARY_SIZE, EMBEDDING_DIMENSION))
+    firstlstm.add(LSTM(EMBEDDING_DIMENSION, HIDDEN_DIMENSION))
+
+    secondlstm = Sequential()
+    secondlstm.add(Embedding(VOCABULARY_SIZE, EMBEDDING_DIMENSION))
+    secondlstm.add(LSTM(EMBEDDING_DIMENSION, HIDDEN_DIMENSION))
+
+    model = Sequential()
+    model.add(Merge([firstlstm, secondlstm], mode='concat'))
+    model.add(Dense(HIDDEN_DIMENSION + HIDDEN_DIMENSION, 1))
+    model.add(Activation('sigmoid'))
+    print('Compiling...', file=sys.stderr)
+    model.compile(loss='binary_crossentropy', optimizer='adam', class_mode="binary")
+
+    print('Training...', file=sys.stderr)
+    model.fit([X1, X2], y, batch_size=BATCH_SIZE, nb_epoch=EPOCHS,
+              validation_split=0.05, show_accuracy=True)
+
+    print("Testing...", file=sys.stderr)
+    score, acc = model.evaluate([X1_test, X2_test], y_test, batch_size=BATCH_SIZE,
+                                show_accuracy=True)
+    print("Testing performance = " + str(score) + ", acc = " + str(acc))
 
 if __name__ == "__main__":
-    main()
+    main_separatemodels()
