f42225d397710ec19ad044d5438ff54dd196da0e
DenisSavenkov <denissavenkov@gmail.com>
2016-07-11 11:32:58 -0400

    Adding timer

--- code/dsavenk/Wikification/src/main/scala/edu/emory/mathcs/ir/utils/YahooAnswersReader.scala
+++ code/dsavenk/Wikification/src/main/scala/edu/emory/mathcs/ir/utils/YahooAnswersReader.scala
@@ -13,8 +13,10 @@ object YahooAnswersReader {
 
   def main(args: Array[String]): Unit = {
     val input = new YahooWebScopeXmlInput(args(0))
-    val out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(args(1))))  
+    val out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(args(1))))
     try {
+      var counter = 0
+      val start = System.nanoTime()
       for (qna <- input.asScala) {
         val question = (qna.questionTitle + "\n" + qna.questionBody).replace("\n", " ").replace("\t", " ")
         val answer = qna.bestAnswer.replace("\n", " ").replace("\t", " ")
@@ -25,7 +27,9 @@ object YahooAnswersReader {
         mentions.map(mention => mention.entity + "\t" + mention.start + "\t" +
             mention.end + "\t" + mention.rho + "\t" + mention.coherence + "\t" +
             mention.votes)
-          .foreach(println(_))
+          .foreach(out.println)
+        counter += 1
+        if (counter % 100 == 0) println(counter + " questions processed..." + (1e-9 * (System.nanoTime() - start) / counter) + " seconds per questions")
       }
     } finally {
       out.close()
