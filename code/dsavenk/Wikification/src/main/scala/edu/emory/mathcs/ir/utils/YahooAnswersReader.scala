b40f991f2833c974380345f2caf068999e8fb3a4
DenisSavenkov <denissavenkov@gmail.com>
2016-07-11 15:25:19 -0400

    Filtering out questions with body

--- code/dsavenk/Wikification/src/main/scala/edu/emory/mathcs/ir/utils/YahooAnswersReader.scala
+++ code/dsavenk/Wikification/src/main/scala/edu/emory/mathcs/ir/utils/YahooAnswersReader.scala
@@ -13,20 +13,20 @@ object YahooAnswersReader {
 
   def main(args: Array[String]): Unit = {
     val input = new YahooWebScopeXmlInput(args(0))
-    val out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(args(1))))
+    val out = new PrintWriter(new GZIPOutputStream(new BufferedOutputStream(new FileOutputStream(args(1)))))
     try {
       var counter = 0
       val start = System.nanoTime()
-      for (qna <- input.asScala) {
-        val question = (qna.questionTitle + "\n" + qna.questionBody).replace("\n", " ").replace("\t", " ")
+      for (qna <- input.asScala if qna.questionBody.isEmpty) {
+        val question = qna.questionTitle.replace("\n", " ").replace("\t", " ")
         val answer = qna.bestAnswer.replace("\n", " ").replace("\t", " ")
         val fullText = question + "?\t" + answer
         val mentions = TagMeWikifier.getEntityMentions(fullText)
         out.println(fullText)
         out.println(mentions.length)
         mentions.map(mention => mention.entity + "\t" + mention.start + "\t" +
-            mention.end + "\t" + mention.rho + "\t" + mention.coherence + "\t" +
-            mention.votes)
+            mention.end + "\t" + fullText.substring(mention.start, mention.end) +
+            mention.rho + "\t" + mention.coherence + "\t" + mention.votes)
           .foreach(out.println)
         counter += 1
         if (counter % 100 == 0) println(counter + " questions processed..." + (1e-9 * (System.nanoTime() - start) / counter) + " seconds per questions")
